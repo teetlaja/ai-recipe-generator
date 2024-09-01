337a74dbc8f38de7fcbe969023124c2859a82e11
Teet Laja <teet@codelinestudio.eu>
2024-09-01 17:49:31 +0300

    Add Bedrock integration with new data source and schema
    Introduced a new `bedrock.js` file for handling Bedrock API requests and responses. Updated `backend.ts` and `resource.ts` to define a new data source and schema, enabling Bedrock model invocation.

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,29 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
+const schema = a.schema({
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a
+  .query()
+  .arguments({ ingredients: a.string().array() })
+  .returns(a.ref("BedrockResponse"))
+  .authorization((allow) => [allow.authenticated()])
+  .handler(
+    a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
+  ),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
